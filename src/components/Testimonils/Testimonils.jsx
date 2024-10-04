f218b4669f89b8b8d0778b11745f6a70919cb280
shuva9800 <ffshuva@gmail.com>
2024-10-04 17:38:18 +0530

    Add testimonils section

--- src/components/Testimonils/Testimonils.jsx
+++ src/components/Testimonils/Testimonils.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
-import "./Testimonils.css"
+import React from "react";
+import "./Testimonils.css";
+import rectangle19 from "../../assets/Rectangle19.png";
+import rectangle20 from "../../assets/Rectangle20.png";
+import rectangle21 from "../../assets/Rectangle21.png";
 
 export default function Testimonils() {
   return (
-    <div>Testimonils</div>
-  )
+    <div className="testimonils">
+      <div className="wrapper">
+        <h2 className="testimonils-heading">What our students say?</h2>
+        <div className="description">
+          <div className="details">
+            <img src={rectangle19} />
+            <h3>Peter Adams</h3>
+            <p>This is a great course. I got to learn a lot.</p>
+          </div>
+          <div className="details">
+            <img src={rectangle20} />
+            <h3>Robert Fox</h3>
+            <p>
+              I got to learn a lot about Music Production with this course.
+              Thanks :)
+            </p>
+          </div>
+          <div className="details">
+            <img src={rectangle21} />
+            <h3>Emily Smith</h3>
+            <p>Awesome! Great job!!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Topics/Topics.jsx
+++ src/components/Topics/Topics.jsx
@@ -1,24 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Topics.css";
 import topicImg from "../../assets/Rectangle 13.jpg";
+import daw from "../../assets/Rectangle 14.jpg";
+import vocal from "../../assets/Rectangle 15.jpg";
+import mixing from "../../assets/Rectangle 17.jpg";
+import mixingconsole from "../../assets/Rectangle 16.jpg";
+import mastring from "../../assets/Rectangle 18.jpg";
 import rectangle from "../../assets/Group 7.svg"
 
 const Topics = () => {
+  const [image , setImage]= useState(topicImg);
   return (
     <section className="topics-section">
       <div className="wrapper">
         <h2 className="topics-heading">What will you learn?</h2>
         <div className="topics-details">
           <ul>
-            <li>What are frequencies?</li>
-            <li>Using DAW</li>
-            <li>Vocals Processing</li>
-            <li>Mixing</li>
-            <li>Mixing Console</li>
-            <li>Mastering</li>
+            <li onMouseEnter={()=> setImage(topicImg)}>What are frequencies?</li>
+            <li onMouseEnter={()=> setImage(daw)}>Using DAW</li>
+            <li onMouseEnter={()=> setImage(vocal)}>Vocals Processing</li>
+            <li onMouseEnter={()=> setImage(mixing)}>Mixing</li>
+            <li onMouseEnter={()=> setImage(mixingconsole)}>Mixing Console</li>
+            <li onMouseEnter={()=> setImage(mastring)}>Mastering</li>
           </ul>
           <div className="topics-details-img">
-            <img src={topicImg} className="computer-img"/>
+            <img src={image} className="computer-img"/>
             <img src={rectangle} className="rectangle"/>
           </div>
         </div>
