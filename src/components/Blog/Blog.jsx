baa4b4241bd0249ad244c8be15b5563454b1aa5a
shuva9800 <ffshuva@gmail.com>
2024-10-05 00:06:03 +0530

    add blog and footer section

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import "./Blog.css"
+import React from "react";
+import "./Blog.css";
+import topicImg from "../../assets/Thumbnail1.png";
+import daw from "../../assets/Thumbnail2.png";
+import vocal from "../../assets/Thumbnail3.png";
 
 export default function Blog() {
   return (
-    <div>Blog</div>
-  )
+    <section className="blog-section">
+      <div className="wrapper">
+        <h2 className="blog-heading">Latest Posts</h2>
+        <div className="all-blogs">
+          <div className="blog-card">
+            <img src={topicImg} />
+            <p className="tag">DAW</p>
+            <p className="card-details">
+              How To Use Drum Machine in Logic Pro X
+            </p>
+          </div>
+          <div className="blog-card">
+            <img src={daw} />
+            <p className="tag">Mixing</p>
+            <p className="card-details">How To Mix Guitars Effectively</p>
+          </div>
+          <div className="blog-card">
+            <img src={vocal} />
+            <p className="tag">VOX</p>
+            <p className="card-details">
+              The Real Power of Harmonies in Music Production
+            </p>
+          </div>
+        </div>
+        <div className="all-posts">
+          <p>All Posts</p>
+        </div>
+      </div>
+    </section>
+  );
 }
-
-
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
-import "./Footer.css"
+import React from "react";
+import "./Footer.css";
+import facebook from "../../assets/Facebook Logo.svg";
+import instagram from "../../assets/Instagram Logo.svg";
+import twitter from "../../assets/Twitter Logo.svg";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer>
+      <div className="wrapper">
+        <div className="wrapper-footer">
+          <div className="footer-details">
+            <p href="#" className="footer-para">
+              Sound <span>DZing</span>
+            </p>
+            <div className="footer-socialmedia">
+              <img src={facebook} />
+              <img src={twitter} />
+              <img src={instagram} />
+            </div>
+          </div>
+          <div className="footer-details footer-contant">
+            <p>Quick Links</p>
+            <p>Blog</p>
+            <p>Privacy Policy</p>
+          </div>
+          <div className="footer-details footer-contant">
+            <p>Contact Us</p>
+            <p>contact@email.com</p>
+            <p>+9800527110</p>
+          </div>
+        </div>
+        <p className="developed-by">
+          This website is designed by <span>Shuvankar Bhowmik</span>{" "}
+        </p>
+      </div>
+    </footer>
+  );
 }
